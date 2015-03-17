244e03ca68f999b6790207333a4909f5c16a1476
Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
2015-03-17 16:10:02 -0700

    CDH-26149: No navigator lineage for CREATE/ALTER VIEW statements
    This commit fixes the issue where no lineage events are generated for
    create and alter view statements.
    
    Change-Id: Ib8c4513219569f62eb26a0eb09a8c2a762054b70
    Reviewed-on: http://gerrit.cloudera.org:8080/265
    Reviewed-by: Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
    Tested-by: Internal Jenkins

--- fe/src/main/java/com/cloudera/impala/analysis/CreateOrAlterViewStmtBase.java
+++ fe/src/main/java/com/cloudera/impala/analysis/CreateOrAlterViewStmtBase.java
@@ -28,6 +28,9 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Base class for CREATE VIEW and ALTER VIEW AS SELECT statements.
  */
