da21e75aedebb2f799094b17182f169cfa09bed9
Martin Grund <mgrund@cloudera.com>
2015-04-08 09:08:12 -0700

    IMPALA-1588: Enabling caching of HDFS file handles (Part III)
    This patch enables caching of HDFS file handles to avoid opening the
    file over and over again. When a file is opened for the first time, a
    HdfsCachedFileHandle object is created that is a small wrapper around
    the hdfsFile instance allowing to associate the last modified timestamp
    with this instance.
    
    When the file handle is no longer needed, it is returned to the
    DiskIoMgr where it is cached under the given path. When the file is
    opened again, first a lookup is performed to see if an existing handle
    can be reused. If there is an existing handle, the last modified time
    of the cached handle is compared with the last modified time of the file
    to be opened. If they are equal the handle can be reused, otherwise it
    is closed and the file is opened regularly.
    
    The new flag `-max_cached_file_handles` controls the overall size of the
    cache by defining an upper bound of cached file handles. Furthermore,
    five new metrics were added to report the number of currently cached
    file handles in the DiskIoMgr and the hit ratio of the cache (including
    hit and miss count).
    
            impala-server.io.mgr.num-cached-file-handles
    
            impala-server.io.mgr.cached-file-handles-hit-ratio
    
            impala-server.io.mgr.cached-file-handles-hit-count
    
            impala-server.io.mgr.cached-file-handles-miss-count
    
            mpala-server.io.mgr.num-file-handles-outstanding
    
    Due to the way how Impala performs the scan operations the cache may
    contain multiple entries for the same file. If the limit of open files
    in the context of the process is smaller than `max_cached_file_handles`,
    the lower limit is used as the cache capacity.
    
    Performance and Memory Evaluation:
    
    The patch was evaluated in three tests
    
    1) Throughput, parallel scans on a small table with 200 small files. TP
    increased from ~50 QPS to ~150 QPS with FD caching.
    
    2) Latency: single table with 300k files. Running select count(*) on the
    table was executed in 2792.30s with FD caching and in 2764.81s without
    FD caching (based HEAD~1 commit). No overhead.
    
    3) Memory consumption. For the above table the delta in RSS memory
    consumption after running the query is 30MB which equals roughly the
    expected 2-3kB per FD for 10k cached descriptors.
    
    Change-Id: Ifa6560d141188c329d7bc73c2dabcc1352d69cd7
    Reviewed-on: http://gerrit.cloudera.org:8080/366
    Tested-by: Internal Jenkins
    Reviewed-by: Martin Grund <mgrund@cloudera.com>

--- be/src/runtime/disk-io-mgr-scan-range.cc
+++ be/src/runtime/disk-io-mgr-scan-range.cc
@@ -279,19 +279,16 @@ Status DiskIoMgr::ScanRange::Open() {
 
   if (fs_ != NULL) {
     if (hdfs_file_ != NULL) return Status::OK();
-
-    // TODO: is there much overhead opening hdfs files?  Should we try to preserve
-    // the handle across multiple scan ranges of a file?
-    hdfs_file_ = hdfsOpenFile(fs_, file(), O_RDONLY, 0, 0, 0);
+    hdfs_file_ = io_mgr_->OpenHdfsFile(fs_, file(), mtime());
     QUERY_VLOG_FILE(reader_->logger()) << "hdfsOpenFile() file=" << file();
     if (hdfs_file_ == NULL) {
       return Status(GetHdfsErrorMsg("Failed to open HDFS file ", file_));
     }
 
     QUERY_VLOG_FILE(reader_->logger()) << "hdfsSeek() offset=" << offset_;
-    if (hdfsSeek(fs_, hdfs_file_, offset_) != 0) {
-      hdfsCloseFile(fs_, hdfs_file_);
+    if (hdfsSeek(fs_, hdfs_file_->file(), offset_) != 0) {
       QUERY_VLOG_FILE(reader_->logger()) << "hdfsCloseFile() (error) file=" << file();
+      io_mgr_->CacheOrCloseFileHandle(file(), hdfs_file_, false);
       hdfs_file_ = NULL;
       string error_msg = GetHdfsErrorMsg("");
       stringstream ss;
@@ -331,7 +328,7 @@ void DiskIoMgr::ScanRange::Close() {
 
     struct hdfsReadStatistics* stats;
     if (IsDfsPath(file())) {
-      int success = hdfsFileGetReadStatistics(hdfs_file_, &stats);
+      int success = hdfsFileGetReadStatistics(hdfs_file_->file(), &stats);
       if (success == 0) {
         reader_->bytes_read_local_ += stats->totalLocalBytesRead;
         reader_->bytes_read_short_circuit_ += stats->totalShortCircuitBytesRead;
@@ -351,11 +348,12 @@ void DiskIoMgr::ScanRange::Close() {
       }
     }
     if (cached_buffer_ != NULL) {
-      hadoopRzBufferFree(hdfs_file_, cached_buffer_);
+      hadoopRzBufferFree(hdfs_file_->file(), cached_buffer_);
       cached_buffer_ = NULL;
     }
-    hdfsCloseFile(fs_, hdfs_file_);
+    io_mgr_->CacheOrCloseFileHandle(file(), hdfs_file_, false);
     QUERY_VLOG_FILE(reader_->logger()) << "hdfsCloseFile() file=" << file();
+    VLOG_FILE << "Cache HDFS file handle file=" << file();
     hdfs_file_ = NULL;
   } else {
     if (local_file_ == NULL) return;
@@ -401,7 +399,7 @@ Status DiskIoMgr::ScanRange::Read(char* buffer, int64_t* bytes_read, bool* eosr)
     int64_t max_chunk_size = MaxReadChunkSize();
     while (*bytes_read < bytes_to_read) {
       int chunk_size = min(bytes_to_read - *bytes_read, max_chunk_size);
-      int last_read = hdfsRead(fs_, hdfs_file_, buffer + *bytes_read, chunk_size);
+      int last_read = hdfsRead(fs_, hdfs_file_->file(), buffer + *bytes_read, chunk_size);
       if (last_read == -1) {
         return Status(GetHdfsErrorMsg("Error reading from HDFS file: ", file_));
       } else if (last_read == 0) {
@@ -446,7 +444,8 @@ Status DiskIoMgr::ScanRange::ReadFromCache(bool* read_succeeded) {
 
     DCHECK(hdfs_file_ != NULL);
     DCHECK(cached_buffer_ == NULL);
-    cached_buffer_ = hadoopReadZero(hdfs_file_, io_mgr_->cached_read_options_, len());
+    cached_buffer_ = hadoopReadZero(hdfs_file_->file(),
+        io_mgr_->cached_read_options_, len());
 
     // Data was not cached, caller will fall back to normal read path.
     if (cached_buffer_ == NULL) {
