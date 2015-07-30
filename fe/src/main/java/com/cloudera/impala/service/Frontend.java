0993d0490d2fa07eb785adf1db9a27a35d04288a
Alex Behm <alex.behm@cloudera.com>
2015-07-30 10:44:01 -0700

    Nested Types: Pretty print complex types in DESCRIBE.
    The current DESCRIBE prints the column type as a single string without
    whitespace. As a result, the DESCRIBE output for tables with complex types
    is basically unreadable/unusable, e.g., from the Impala shell.
    
    This patch adds a prettyPrint() function to the FE Type and uses that
    for generating a nicely formatted DESCRIBE output.
    
    The output of DESCRIBE FORMATTED is intentionally not modified because
    exact Hive-compatibility has been and presumably continues to be very
    important to our users.
    
    Change-Id: Ida810facdffd970948b837b83a60f9ddcd95f44d
    Reviewed-on: http://gerrit.cloudera.org:8080/633
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins

--- fe/src/main/java/com/cloudera/impala/service/Frontend.java
+++ fe/src/main/java/com/cloudera/impala/service/Frontend.java
@@ -20,6 +20,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
