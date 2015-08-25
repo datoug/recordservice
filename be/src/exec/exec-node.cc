ibmsoe/ImpalaPPC

Make sure children are created before ExecNode::Init() is called on parent
SubplanNode::Init() expects that its children are already created,
which is not the case before this patch. We could move the work done
in SubplanNode::Init() to SubplanNode::Prepare(), but it seems like a
reasonable invariant of Init() to already have access to Init()'d
children. This patch achieves this invariant by moving the
ExecNode::Init() call from ExecNode::CreateNode() to
ExecNode::CreateTreeHelper().

Change-Id: Ia226d0d4796b96a72bfd9b3be56aa619423e84cf
Reviewed-on: http://gerrit.cloudera.org:8080/593
Reviewed-by: Alex Behm <alex.behm@cloudera.com>
Tested-by: Internal Jenkins

be/src/exec/exec-node.cc
@@ -230,10 +230,11 @@ Status ExecNode::CreateTreeHelper(
   if (*node_idx >= tnodes.size()) {
     return Status("Failed to reconstruct plan tree from thrift.");
   }
-  int num_children = tnodes[*node_idx].num_children;
+  const TPlanNode& tnode = tnodes[*node_idx];
+
+  int num_children = tnode.num_children;
   ExecNode* node = NULL;
-  RETURN_IF_ERROR(CreateNode(pool, tnodes[*node_idx], descs, &node));
-  // assert(parent != NULL || (node_idx == 0 && root_expr != NULL));
+  RETURN_IF_ERROR(CreateNode(pool, tnode, descs, &node));
   if (parent != NULL) {
     parent->children_.push_back(node);
   } else {
@@ -248,6 +249,9 @@ Status ExecNode::CreateTreeHelper(
       return Status("Failed to reconstruct plan tree from thrift.");
     }
   }
+
+  // Call Init() after children have been set and Init()'d themselves
+  RETURN_IF_ERROR(node->Init(tnode));
 
   // build up tree of profiles; add children >0 first, so that when we print
   // the profile, child 0 is printed last (makes the output more readable)
@@ -338,7 +342,6 @@ Status ExecNode::CreateNode(ObjectPool* pool, const TPlanNode& tnode,
       error_msg << str << " not implemented";
       return Status(error_msg.str());
   }
-  RETURN_IF_ERROR((*node)->Init(tnode));
   return Status::OK();
 }
 

datoug/recordservice

be/src/exec/exec-node.cc
@@ -232,10 +232,11 @@ Status ExecNode::CreateTreeHelper(
   if (*node_idx >= tnodes.size()) {
     return Status("Failed to reconstruct plan tree from thrift.");
   }
-  int num_children = tnodes[*node_idx].num_children;
+  const TPlanNode& tnode = tnodes[*node_idx];
+
+  int num_children = tnode.num_children;
   ExecNode* node = NULL;
-  RETURN_IF_ERROR(CreateNode(state, tnodes[*node_idx], descs, &node));
-  // assert(parent != NULL || (node_idx == 0 && root_expr != NULL));
+  RETURN_IF_ERROR(CreateNode(state, tnode, descs, &node));
   if (parent != NULL) {
     parent->children_.push_back(node);
   } else {
@@ -250,6 +251,9 @@ Status ExecNode::CreateTreeHelper(
       return Status("Failed to reconstruct plan tree from thrift.");
     }
   }
+
+  // Call Init() after children have been set and Init()'d themselves
+  RETURN_IF_ERROR(node->Init(tnode));
 
   // build up tree of profiles; add children >0 first, so that when we print
   // the profile, child 0 is printed last (makes the output more readable)
@@ -345,7 +349,6 @@ Status ExecNode::CreateNode(RuntimeState* state, const TPlanNode& tnode,
       error_msg << str << " not implemented";
       return Status(error_msg.str());
   }
-  RETURN_IF_ERROR((*node)->Init(tnode));
   return Status::OK();
 }
 